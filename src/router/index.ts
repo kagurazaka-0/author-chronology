dbe4420b3ea6141d9d05f0431f76afbf21c77bbd
katsuya_U <katsuya_u@icloud.com>
2021-01-22 13:42:09 +0900

    feat: タブを整理

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,10 +23,6 @@ const routes: Array<RouteRecordRaw> = [
         path: "tab2",
         component: () => import("@/views/Tab2.vue"),
       },
-      {
-        path: "tab3",
-        component: () => import("@/views/Tab3.vue"),
-      },
     ],
   },
 ]
--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -3,18 +3,13 @@
     <ion-tabs>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon :icon="searchOutline" />
+          <ion-label>著者検索</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon :icon="ellipsisHorizontalOutline" />
+          <ion-label>その他</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -22,15 +17,14 @@
 </template>
 
 <script lang="ts">
-import { ellipse, square, triangle } from "ionicons/icons"
+import { ellipsisHorizontalOutline, searchOutline } from "ionicons/icons"
 
 export default {
   name: "Tabs",
   setup() {
     return {
-      ellipse,
-      square,
-      triangle,
+      ellipsisHorizontalOutline,
+      searchOutline,
     }
   },
 }
