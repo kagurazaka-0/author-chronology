88f29ed75a15e7fc20a83698f6e428e813c52093
katsuya_U <katsuya_u@icloud.com>
2021-01-22 14:30:36 +0900

    feat: Tab1の最初の画面を実装

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -2,26 +2,51 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>著者検索</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+      <!-- TODO: ここにもっとそれらしいタイトルロゴをおく -->
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">著者検索</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+      <form @submit.prevent="onSubmit">
+        <ion-item>
+          <ion-label position="stacked">著者</ion-label>
+          <ion-input type="text" autocomplete="hidden" v-model="auther"></ion-input>
+        </ion-item>
+
+        <div class="ion-padding">
+          <ion-button expand="block" type="submit" class="ion-no-margin">検索</ion-button>
+        </div>
+      </form>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import ExploreContainer from "@/components/ExploreContainer.vue"
+import { defineComponent, ref } from "vue"
+import { alertController } from "@ionic/vue"
 
-export default {
+export default defineComponent({
   name: "Tab1",
-  components: { ExploreContainer },
-}
+  setup() {
+    const auther = ref("")
+    const onSubmit = async () => {
+      if (!auther.value) {
+        const alert = await alertController.create({
+          header: "エラー",
+          message: "著者を入力してください。",
+          buttons: ["OK"],
+        })
+        alert.present()
+        return
+      }
+    }
+    return { auther, onSubmit }
+  },
+})
 </script>
