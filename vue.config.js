7d77f9ebe473922e43ad6923f998ef4683d367ef
katsuya_U <katsuya_u@icloud.com>
2021-01-24 18:33:59 +0900

    chore: worker-loaderの設定を追記

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,17 @@ const config = {
   devServer: {
     host: "0.0.0.0",
   },
+  chainWebpack: config => {
+    // 参考1: https://github.com/Vibrant-Colors/node-vibrant/tree/develop#webpack-configuration
+    // 参考2: https://cli.vuejs.org/guide/webpack.html#adding-a-new-loader
+    config.module
+      .rule("worker")
+      // Add another loader
+      .test(/\.worker.js$/)
+      .use("worker-loader")
+      .loader("worker-loader")
+      .end()
+  },
 }
 
 module.exports = config
